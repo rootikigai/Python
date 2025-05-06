e4acd3e2910d63ae38e7cc2ef4754cc1bb28a79e
rootikigai <ifykel360@gmail.com>
2025-05-06 19:51:12 +0100

    Program to calculate investment return

---
+++ InvestmentReturn.py
@@ -0,0 +1,11 @@
+r = 7 / 100
+n = 10
+n2 = 20
+n3 = 30
+p = int(input("Enter the principal amount: "))
+a = p*(1 + r)**n
+a2 = p*(1 + r)**n2
+a3 = p*(1 + r)**n3
+print("The value after 10 years is: ", a)
+print("The value after 20 years is: ", a2)
+print("The value after 30 years is: ", a3)
